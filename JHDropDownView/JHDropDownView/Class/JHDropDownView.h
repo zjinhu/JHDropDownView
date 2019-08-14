ff5807d7354c7e9a9ac940556365c0de2996b72e
张金虎 <jinhu.zhang@ikang.com>
2019-08-14 14:43:51 +0800

    release

--- JHDropDownView/JHDropDownView/Class/JHDropDownView.h
+++ JHDropDownView/JHDropDownView/Class/JHDropDownView.h
@@ -13,12 +13,13 @@ NS_ASSUME_NONNULL_BEGIN
 @interface JHDropDownView : UIView
 + (instancetype)dropDown;
 
+//弹出遮罩
 + (void)coverTabbar:(UIView *)contentView
               fromY:(CGFloat)fromY
            canClick:(BOOL)canClick
           showBlock:(dispatch_block_t)showBlock
           hideBlock:(dispatch_block_t)hideBlock;
-
+//判断时候已经存在遮罩
 + (BOOL)hasDropDown;
 /**
  隐藏视图
